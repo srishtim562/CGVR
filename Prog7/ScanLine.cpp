301f90f80f252a38d21f05fa485f76c0bba454ea
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2021-12-05 17:15:33 +0530

    Update and rename Source.cpp to Prog7/ScanLine.cpp

--- Prog7/ScanLine.cpp
+++ Prog7/ScanLine.cpp
@@ -87,7 +87,7 @@ void main(int argc, char **argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
-	glutCreateWindow("Scan - line Algorithm");
+	glutCreateWindow("Scan Line Algorithm");
 	glutDisplayFunc(display);
 	myInit();
 	glutMainLoop();

e5d29826ffdc1cf9e27cd77ea111701d13047f47
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2021-12-09 10:21:27 +0530

    Update CohenHodgemanPolygon.cpp

--- Prog10/CohenHodgemanPolygon.cpp
+++ Prog10/CohenHodgemanPolygon.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <GL/glut.h>
-
-using namespace std;
+#include <stdio.h>
 
 int poly_size, poly_points[20][2], org_poly_size, org_poly_points[20][2], clipper_size, clipper_points[20][2];
 const int MAX_POINTS = 20;
@@ -19,10 +17,9 @@ void drawPoly(int p[][2], int n) {
 int x_intersect(int x1, int y1, int x2, int y2,
 	int x3, int y3, int x4, int y4)
 {
-	int num = (x1*y2 - y1*x2) * (x3-x4) -
-		(x1-x2) * (x3*y4 - y3*x4);
-	int den = (x1-x2) * (y3-y4) - (y1-y2) * (x3-x4);
-	return num/den;
+	int num = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
+	int den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	return num / den;
 }
 
 // Returns y-value of point of intersectipn of 
@@ -30,14 +27,13 @@ int x_intersect(int x1, int y1, int x2, int y2,
 int y_intersect(int x1, int y1, int x2, int y2,
 	int x3, int y3, int x4, int y4)
 {
-	int num = (x1*y2 - y1*x2) * (y3-y4) -
-		(y1-y2) * (x3*y4 - y3*x4);
-	int den = (x1-x2) * (y3-y4) - (y1-y2) * (x3-x4);
-	return num/den;
+	int num = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
+	int den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	return num / den;
 }
 
 // This functions clips all the edges w.r.t one clip edge of clipping area 
-void clip(int poly_points[][2], int &poly_size,
+void clip(int poly_points[][2], int& poly_size,
 	int x1, int y1, int x2, int y2)
 {
 	int new_points[MAX_POINTS][2], new_poly_size = 0;
@@ -46,15 +42,15 @@ void clip(int poly_points[][2], int &poly_size,
 	for (int i = 0; i < poly_size; i++)
 	{
 		// i and k form a line in polygon
-		int k = (i+1) % poly_size;
+		int k = (i + 1) % poly_size;
 		int ix = poly_points[i][0], iy = poly_points[i][1];
 		int kx = poly_points[k][0], ky = poly_points[k][1];
 
 		// Calculating position of first point w.r.t. clipper line
-		int i_pos = (x2-x1) * (iy-y1) - (y2-y1) * (ix-x1);
+		int i_pos = (x2 - x1) * (iy - y1) - (y2 - y1) * (ix - x1);
 
 		// Calculating position of second point w.r.t. clipper line
-		int k_pos = (x2-x1) * (ky-y1) - (y2-y1) * (kx-x1);
+		int k_pos = (x2 - x1) * (ky - y1) - (y2 - y1) * (kx - x1);
 
 		// Case 1 : When both points are inside
 		if (i_pos < 0 && k_pos < 0)
@@ -103,9 +99,9 @@ void clip(int poly_points[][2], int &poly_size,
 	}
 }
 
-void init() 
+void init()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 500.0);
@@ -116,29 +112,28 @@ void init()
 void display()
 {
 	init();
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glColor3f(1.0, 0.0, 0.0);
 	drawPoly(clipper_points, clipper_size);
 
-	glColor3f(0.0f, 1.0f, 0.0f);
+	glColor3f(0.0, 1.0, 0.0);
 	drawPoly(org_poly_points, org_poly_size);
-	//i and k are two consecutive indexes 
 
+	//i and k are two consecutive indices 
 	for (int i = 0; i < clipper_size; i++)
 	{
 		int k = (i + 1) % clipper_size;
 
 		// We pass the current array of vertices, it's size and the end points of the selected clipper line 
-		clip(poly_points, poly_size, clipper_points[i][0],
-			clipper_points[i][1], clipper_points[k][0],
-			clipper_points[k][1]);
+		clip(poly_points, poly_size, clipper_points[i][0], 
+			clipper_points[i][1], clipper_points[k][0], clipper_points[k][1]);
 	}
-	glColor3f(0.0f, 0.0f, 1.0f);
+	glColor3f(0.0, 0.0, 1.0);
 	drawPoly(poly_points, poly_size);
 	glFlush();
 }
 
 //Driver code 
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
 	printf("Enter no. of vertices: ");
 	scanf_s("%d", &poly_size);
@@ -163,7 +158,7 @@ int main(int argc, char* argv[])
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(400, 400);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Polygon Clipping!");
+	glutCreateWindow("Polygon Clipping");
 	glutDisplayFunc(display);
 	glutMainLoop();
 	return 0;

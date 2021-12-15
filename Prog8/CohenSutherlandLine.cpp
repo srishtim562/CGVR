67bb8dc0237a14bb39dd79a958354cd696b27288
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2021-12-15 16:25:15 +0530

    Update CohenSutherlandLine.cpp

--- Prog8/CohenSutherlandLine.cpp
+++ Prog8/CohenSutherlandLine.cpp
@@ -1,10 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<gl/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <gl/glut.h>
 
 #define outcode int
-#define true 1
-#define false 0
 
 double xmin, ymin, xmax, ymax;
 double xvmin, yvmin, xvmax, yvmax;
@@ -146,13 +144,12 @@ void display()
 
 	glFlush();
 }
+
 void myinit()
 {
 	glClearColor(1, 1, 1, 1);
 	glColor3f(1, 0, 0);
 	glPointSize(1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
 	gluOrtho2D(0, 500, 0, 500);
 }
 

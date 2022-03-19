25744beb1abde62f0b689543b09dfb8f24d3e8a6
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2022-03-19 22:17:37 +0530

    Update ScanLine.cpp

--- Prog7/ScanLine.cpp
+++ Prog7/ScanLine.cpp
@@ -8,21 +8,21 @@ float x[100], y[100]; //= { 0,0,20,100,100 }, y[] = { 0,100,50,100,0 };
 int n, m, wx = 500, wy = 500;
 static float intx[10] = { 0 };
 
-void drawLine(float x1, float y1, float x2, float y2) 
+void drawLine(float x1, float y1, float x2, float y2)
 {
 	Sleep(100);
 	glColor3f(1, 0, 0);
 	glBegin(GL_LINES);
-		glVertex2f(x1, y1);
-		glVertex2f(x2, y2);
+	glVertex2f(x1, y1);
+	glVertex2f(x2, y2);
 	glEnd();
 	glFlush();
 }
 
-void edgeDetect(float x1, float y1, float x2, float y2, int scanline) 
+void edgeDetect(float x1, float y1, float x2, float y2, int scanline)
 {
 	float temp;
-	if (y2 < y1) 
+	if (y2 < y1)
 	{
 		temp = x1; x1 = x2; x2 = temp;
 		temp = y1; y1 = y2; y2 = temp;
@@ -32,12 +32,12 @@ void edgeDetect(float x1, float y1, float x2, float y2, int scanline)
 		intx[m++] = x1 + (scanline - y1) * (x2 - x1) / (y2 - y1);
 }
 
-void scanFill(float x[], float y[]) 
+void scanFill(float x[], float y[])
 {
-	for (int s1 = 0; s1 <= wy; s1++) 
+	for (int s1 = 0; s1 <= wy; s1++)
 	{
 		m = 0;
-		for (int i = 0; i < n; i++) 
+		for (int i = 0; i < n; i++)
 			edgeDetect(x[i], y[i], x[(i + 1) % n], y[(i + 1) % n], s1);
 
 		sort(intx, (intx + m));
@@ -47,10 +47,8 @@ void scanFill(float x[], float y[])
 	}
 }
 
-void displayFilledPolygon() 
+void displayFilledPolygon()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glLineWidth(2);
 	glBegin(GL_LINE_LOOP);
 	for (int i = 0; i < n; i++)
 		glVertex2f(x[i], y[i]);
@@ -59,15 +57,16 @@ void displayFilledPolygon()
 	glFlush();
 }
 
-void myInit() 
+void myInit()
 {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(0.0, 0.0, 1.0);
 	glPointSize(1);
 	gluOrtho2D(0, wx, 0, wy);
 }
 
-void main(int argc, char **argv) 
+void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	printf("Enter no. of sides: ");
@@ -80,9 +79,8 @@ void main(int argc, char **argv)
 	}
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Scan Line");
-	glutDisplayFunc(displayFilledPolygon);
 	myInit();
+	glutDisplayFunc(displayFilledPolygon);
 	glutMainLoop();
 }

21791ca8b949051b355096c42984cef0f0ab2403
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2022-03-19 11:15:37 +0530

    Update BresenhamCircle.cpp

--- Prog2/BresenhamCircle.cpp
+++ Prog2/BresenhamCircle.cpp
@@ -1,18 +1,21 @@
-#include <GL/glut.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
 #include <corecrt_math.h>
 
-int xc, yc, r, flag = 0, n, i;
-int x1, yc1, x2, y2;
+int r, xc, yc, x2, y2, flag, n;
 
-void drawPixel(GLint cx, GLint cy)
+void drawPixel(int x, int y)
 {
+	glColor3f(1, 0, 0);
+	glPointSize(3.0);
 	glBegin(GL_POINTS);
-		glVertex2i(cx, cy);
+		glVertex2i(x, y);
 	glEnd();
+	glFlush();
 }
 
-void plotPixels(GLint h, GLint k, GLint x, GLint y)
+void plotPixels(int h, int k, int x, int y)
 {
 	drawPixel(x + h, y + k);
 	drawPixel(-x + h, y + k);
@@ -24,32 +27,32 @@ void plotPixels(GLint h, GLint k, GLint x, GLint y)
 	drawPixel(-y + h, -x + k);
 }
 
-void drawCircle(GLint h, GLint k, GLint r)	//Bresenham's circle drawing algorithm
+void drawCircle(int h, int k, int r)
 {
-	GLint d = 3 - 2 * r, x = 0, y = r;
+	int d = 3 - 2 * r, x = 0, y = r;
 
 	while (y > x)
 	{
 		plotPixels(h, k, x, y);
 		if (d < 0)
-			d = d + 4 * x + 6;
+			d += 4 * x + 6;
 		else
 		{
-			d = d + 4 * (x - y) + 10;
-			--y;
+			d += 4 * (x - y) + 10;
+			y--;
 		}
-		++x;
+		x++;
+		plotPixels(h, k, x, y);
 	}
-	plotPixels(h, k, x, y);
-	glFlush();
 }
 
 void myMouse(int button, int state, int x, int y)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && flag == 0)
-	{
-		x1 = x - 250;
-		yc1 = 250 - y;
+	{ 
+		xc = x - 250;
+		yc = 250 - y;
+		printf("Defining xc, yc = (%d, %d)\n", xc, yc);
 		flag = 1;
 	}
 
@@ -57,13 +60,11 @@ void myMouse(int button, int state, int x, int y)
 	{
 		x2 = x - 250;
 		y2 = 250 - y;
-		xc = x1;
-		yc = yc1;
-		float exp = ((x2 - x1) * (x2 - x1)) + ((y2 - yc1) * (y2 - yc1));
-		r = (int)(sqrt(exp));
-		printf("Defining centre = (%d, %d), radius = %d\n", xc, yc, r);
-		drawCircle(xc, yc, r);
+		float exp = (x2 - xc) * (x2 - xc) + (y2 - yc) * (y2 - yc);
+		r = (int)sqrt(exp);
+		printf("Defining r = %d\n", r);
 		flag = 0;
+		drawCircle(xc, yc, r);
 	}
 }
 
@@ -72,47 +73,48 @@ void displayMouse()
 
 void displayKeyboard()
 {
-	for (i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		printf("Enter coordinates of the center (X, Y), and the radius (R):\n");
+		printf("Enter xc, yc, r\n");
 		scanf_s("%d %d %d", &xc, &yc, &r);
 		drawCircle(xc, yc, r);
 	}
 }
 
-void myInit()
+void init()
 {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glColor3f(0.0, 1.0, 0.0);
-	glPointSize(3.0);
-	gluOrtho2D(-250.0, 250.0, -250.0, 250.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(1, 1, 1, 1);
+	gluOrtho2D(-250, 250, -250, 250);
 }
 
-int main(int argc, char** argv)
+void main(int argc, char** argv)
 {
 	int choice;
-	printf("Enter 1 for keyboard, 2 for mouse: ");
+	printf("Press 1 for keyboard, 2 for mouse: ");
 	scanf_s("%d", &choice);
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 
 	switch (choice)
 	{
-		case 1:	printf("Enter the number of circles: ");
+		case 1:		printf("Enter the number of circles: ");
 				scanf_s("%d", &n);
-				glutCreateWindow("Bresenham's Circle Keyboard");
-				myInit();
+				glutCreateWindow("Bresenham's Circle: Keyboard");
+				init();
 				glutDisplayFunc(displayKeyboard);
 				break;
 
-		case 2: glutCreateWindow("Bresenham's Circle Mouse");
-				myInit();
+		case 2: 	glutCreateWindow("Bresenham's Circle: Mouse");
+				init();
 				glutMouseFunc(myMouse);
 				glutDisplayFunc(displayMouse);
 				break;
 
-		default: printf("Invalid choice\n");
+		default: 	printf("Invalid choice\n");
+				exit(0);
 	}
 	glutMainLoop();
 }

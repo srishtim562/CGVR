3799da87206ff2b1915ba969c0e503321f97b29d
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2022-03-20 17:47:05 +0530

    Update Cube2.cpp

--- Prog12/Cube2.cpp
+++ Prog12/Cube2.cpp
@@ -24,6 +24,7 @@ void delay(float secs)
 void display()
 {
 	// display callback, clear frame buffer and z buffer, rotate cube and draw, swap buffers 
+	glClearColor(1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 

d9784aaaec49ace9f5da9bfe801e8712948660c6
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2022-03-21 11:44:34 +0530

    Update LiangBarsky.cpp

--- Prog9/LiangBarsky.cpp
+++ Prog9/LiangBarsky.cpp
@@ -3,14 +3,13 @@
 
 double xmin, ymin, xmax, ymax; //50 50 100 100
 double xvmin, yvmin, xvmax, yvmax; //200 200 300 300
-
 int n;
 
-struct line_segment {
+struct lineSegment {
 	int x1, y1, x2, y2;
 };
 
-struct line_segment ls[10];
+struct lineSegment ls[10];
 
 int cliptest(double p, double q, double* u1, double* u2)
 {
@@ -42,9 +41,9 @@ int cliptest(double p, double q, double* u1, double* u2)
 	return(true);
 }
 
-void LiangBarskyLineClipAndDraw(double x0, double y0, double x1, double y1)
+void LiangBarskyLineClipAndDraw(double x1, double y1, double x2, double y2)
 {
-	double dx = x1 - x0, dy = y1 - y0, u1 = 0.0, u2 = 1.0;
+	double dx = x2 - x1, dy = y2 - y1, u1 = 0.0, u2 = 1.0;
 	//draw a red colored viewport
 	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_LINE_LOOP);
@@ -53,40 +52,48 @@ void LiangBarskyLineClipAndDraw(double x0, double y0, double x1, double y1)
 		glVertex2f(xvmax, yvmax);
 		glVertex2f(xvmin, yvmax);
 	glEnd();
-	if (cliptest(-dx, x0 - xmin, &u1, &u2))  // inside test wrt left edge
-		if (cliptest(dx, xmax - x0, &u1, &u2)) // inside test wrt right edge
-			if (cliptest(-dy, y0 - ymin, &u1, &u2)) // inside test wrt bottom edge
-				if (cliptest(dy, ymax - y0, &u1, &u2)) // inside test wrt top edge
+	if (cliptest(-dx, x1 - xmin, &u1, &u2))  // inside test wrt left edge
+		if (cliptest(dx, xmax - x1, &u1, &u2)) // inside test wrt right edge
+			if (cliptest(-dy, y1 - ymin, &u1, &u2)) // inside test wrt bottom edge
+				if (cliptest(dy, ymax - y1, &u1, &u2)) // inside test wrt top edge
 				{
 					if (u2 < 1.0)
 					{
-						x1 = x0 + u2 * dx;
-						y1 = y0 + u2 * dy;
+						x2 = x1 + u2 * dx;
+						y2 = y1 + u2 * dy;
 					}
 					if (u1 > 0.0)
 					{
-						x0 = x0 + u1 * dx;
-						y0 = y0 + u1 * dy;
+						x1 = x1 + u1 * dx;
+						y1 = y1 + u1 * dy;
 					}
 					// Window to viewport mappings
 					double sx = (xvmax - xvmin) / (xmax - xmin); // Scale parameters
 					double sy = (yvmax - yvmin) / (ymax - ymin);
-					double vx0 = xvmin + (x0 - xmin) * sx;
-					double vy0 = yvmin + (y0 - ymin) * sy;
 					double vx1 = xvmin + (x1 - xmin) * sx;
 					double vy1 = yvmin + (y1 - ymin) * sy;
+					double vx2 = xvmin + (x2 - xmin) * sx;
+					double vy2 = yvmin + (y2 - ymin) * sy;
 
 					glColor3f(0.0, 0.0, 1.0); // draw blue colored clipped line
 					glBegin(GL_LINES);
-						glVertex2d(vx0, vy0);
 						glVertex2d(vx1, vy1);
+						glVertex2d(vx2, vy2);
 					glEnd();
 				}
 }// end of line clipping
 
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	//draw a blue colored window
+	glColor3f(0, 0, 1);
+	glBegin(GL_LINE_LOOP);
+		glVertex2f(xmin, ymin);
+		glVertex2f(xmax, ymin);
+		glVertex2f(xmax, ymax);
+		glVertex2f(xmin, ymax);
+	glEnd();
+
 	//draw the line with red color
 	glColor3f(1.0, 0.0, 0.0);
 	for (int i = 0; i < n; i++)
@@ -96,14 +103,7 @@ void display()
 			glVertex2d(ls[i].x2, ls[i].y2);
 		glEnd();
 	}
-	//draw a blue colored window
-	glColor3f(0.0, 0.0, 1.0);
-	glBegin(GL_LINE_LOOP);
-		glVertex2f(xmin, ymin);
-		glVertex2f(xmax, ymin);
-		glVertex2f(xmax, ymax);
-		glVertex2f(xmin, ymax);
-	glEnd();
+	
 	for (int i = 0; i < n; i++)
 		LiangBarskyLineClipAndDraw(ls[i].x1, ls[i].y1, ls[i].x2, ls[i].y2);
 	glFlush();
@@ -112,14 +112,12 @@ void display()
 void myinit()
 {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 0.0, 0.0);
-	glLineWidth(2.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+	gluOrtho2D(0, 500, 0, 500);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -138,6 +136,7 @@ int main(int argc, char** argv)
 		printf("Enter coordinates: (x1 y1 x2 y2):\n");
 		scanf_s("%d%d%d%d", &ls[i].x1, &ls[i].y1, &ls[i].x2, &ls[i].y2);
 	}
+
 	glutCreateWindow("Liang Barsky Line Clipping Algorithm");
 	glutDisplayFunc(display);
 	myinit();
